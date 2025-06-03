a4fb100bf407c115e3bd5ea05c07c0ac760303db
alex7-77 <alex.nguyen36@education.nsw.gov.au>
2025-06-03 13:25:30 +1000

    user_management.py

--- user_management.py
+++ user_management.py
@@ -1,14 +1,16 @@
 import sqlite3 as sql
 import time
 import random
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def insertUser(username, password, DoB):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
+    hash = generate_password_hash(password)
     cur.execute(
         "INSERT INTO users (username,password,dateOfBirth) VALUES (?,?,?)",
-        (username, password, DoB),
+        (username, hash, DoB),
     )
     con.commit()
     con.close()
@@ -17,17 +19,17 @@ def insertUser(username, password, DoB):
 def retrieveUsers(username, password):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
+    cur.execute(f'SELECT * FROM users WHERE username = ?', (username,))
     if cur.fetchone() == None:
         con.close()
         return False
     else:
-        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
+        cur.execute(f'SELECT * FROM users WHERE password = ?',(password,))
         # Plain text log of visitor count as requested by Unsecure PWA management
-        with open("visitor_log.txt", "r") as file:
+        with open('visitor_log.txt', 'r') as file:
             number = int(file.read().strip())
             number += 1
-        with open("visitor_log.txt", "w") as file:
+        with open('visitor_log.txt', 'w') as file:
             file.write(str(number))
         # Simulate response time of heavy app for testing purposes
         time.sleep(random.randint(80, 90) / 1000)
